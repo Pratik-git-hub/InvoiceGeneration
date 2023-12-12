f7ae23e3a261b225df0268d1f9c17b104dd09135
Pratik-git-hub <pratikcmkulkarni@gmail.com>
2023-12-13 00:35:23 +0530

    df

--- invoiceGenerationBackend/controllers/generateInvoice.js
+++ invoiceGenerationBackend/controllers/generateInvoice.js
@@ -7,7 +7,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const generateInvoice = async (req, res) => {
-
+  console.log("hiii inside generateinvoice");
   let flag = await createPDF(
     req.body.data.InvoiceNo,
     req.body.data,
