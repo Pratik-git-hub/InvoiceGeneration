d1012638a2bb02e214e443d972ff7a7a18055016
Pratik-git-hub <pratikcmkulkarni@gmail.com>
2023-12-19 14:40:19 +0530

    working code

--- invoiceGenerationBackend/index.js
+++ invoiceGenerationBackend/index.js
@@ -4,12 +4,13 @@ import viewInvoice from "./controllers/viewInvoice.js";
 import cors  from 'cors';
 
 let app = express();
+let port = 8000;
 app.use(express.json());
 app.use(cors())
 app.post("/generate", generateInvoice);
 
 app.post("/view", viewInvoice);
 
-app.listen(8080, function () {
-  console.log("Example app listening on port 3000!");
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}!`);
 });
